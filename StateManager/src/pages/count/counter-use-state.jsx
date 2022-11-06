231be8d9d360863b7bd8f8c0e252167db275c183
JiangShuuu <globelex65@gmail.com>
2022-11-06 15:37:07 +0800

    upd

--- StateManager/src/pages/count/counter-use-state.jsx
+++ StateManager/src/pages/count/counter-use-state.jsx
@@ -1,91 +1,23 @@
-import { useReducer, createContext, useContext } from "react";
+import { useState } from "react";
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "add":
-      return state + 1;
-    case "reduce":
-      return state - 1;
-    default:
-      return state;
-  }
-};
+const Container = ({ children }) => <div>{children}</div>;
 
-const CounterContext = createContext(null);
-
-const CounterContextProvider = ({ children }) => {
-  const [counter, dispatch] = useReducer(reducer, 0);
-
-  return (
-    <CounterContext.Provider value={{ counter, dispatch }}>
-      {children}
-    </CounterContext.Provider>
-  );
-};
-
-const Container = () => (
+const AddOneButton = ({ setCounter }) => (
   <div>
-    <AddOneButton />
-    <ReduceButton />
+    <button onClick={() => setCounter((v) => v + 1)}>Add One</button>
   </div>
 );
 
-const AddOneButton = () => {
-  const { dispatch } = useContext(CounterContext);
+const Counter = ({ counter }) => <div>Counter: {counter}</div>;
 
+export default function CounterUseState() {
+  const [counter, setCounter] = useState(0);
   return (
     <div>
-      <button
-        onClick={() =>
-          dispatch({
-            type: "add"
-          })
-        }
-      >
-        Add One
-      </button>
-    </div>
-  );
-};
-
-const ReduceButton = () => {
-  const { dispatch } = useContext(CounterContext);
-  return (
-    <div>
-      <button
-        onClick={() =>
-          dispatch({
-            type: "reduce"
-          })
-        }
-      >
-        Reduce One
-      </button>
-    </div>
-  );
-};
-
-const Counter = () => {
-  const { counter } = useContext(CounterContext);
-  return <div>Counter: {counter}</div>;
-};
-
-function CounterUseState() {
-  return (
-    <CounterContextProvider>
-      <Container />
-      <Counter />
-    </CounterContextProvider>
-  );
-}
-
-export default function CounterUseStatePage() {
-  return (
-    <div>
-      <CounterUseState />
-      <CounterUseState />
-      <CounterUseState />
-      <CounterUseState />
+      <Container>
+        <AddOneButton setCounter={setCounter} />
+      </Container>
+      <Counter counter={counter} />
     </div>
   );
 }
