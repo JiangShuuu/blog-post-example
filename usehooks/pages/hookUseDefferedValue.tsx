3f4721893feb3a85478fda1f5d85648d0a72ccf8
JiangShuuu <globelex65@gmail.com>
2022-11-20 17:07:12 +0800

    upd

--- usehooks/pages/hookUseDefferedValue.tsx
+++ usehooks/pages/hookUseDefferedValue.tsx
@@ -2,9 +2,10 @@ import React, { useMemo, useState, useDeferredValue, useEffect } from 'react';
 import Link from 'next/link';
 
 function Lists({ input }: { input: string }) {
-  const LIST_SIZE = 20000;
+  const LIST_SIZE = 10000;
   const defferedInput = useDeferredValue(input);
 
+  // useMemo 用於每次畫面re-Render時, 若結果一樣則不再次 render
   const list = useMemo(() => {
     const product = [];
     for (let i = 0; i < LIST_SIZE; i++) {
@@ -14,7 +15,7 @@ function Lists({ input }: { input: string }) {
   }, [defferedInput]);
 
   useEffect(() => {
-    console.log(`Input: ${input}\nDeffered: ${defferedInput}`);
+    console.log(`Input: ${input}\n Deffered: ${defferedInput}`);
   }, [input, defferedInput]);
 
   return list;
