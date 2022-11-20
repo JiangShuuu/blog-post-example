b33f746d2f8a2d7e90cc8d1f5b2e25590c334462
JiangShuuu <globelex65@gmail.com>
2022-11-20 16:04:18 +0800

    upd

--- usehooks/pages/hookUseId.tsx
+++ usehooks/pages/hookUseId.tsx
@@ -11,14 +11,20 @@ const Hook = () => {
   );
 };
 
-export default function HookUseId() {
-  const id = useId();
+export default function HookUseId({ data }: any) {
+  // random 會因為 Hydration 前後而造成 id random 不一至狀況
+  // const id = Math.random();
   return (
     <div className='content'>
       <h1>hookUseId</h1>
       <header>
         <Link href='/'>Home</Link>
       </header>
+
+      {/* <div>
+        <label htmlFor={`${id}`}>data</label>
+        <input id={id} type='text' />
+      </div> */}
 
       <div>
         <Hook />
